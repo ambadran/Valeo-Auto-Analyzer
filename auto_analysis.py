ac74972e35acc313919dc81798168d30f02e3a4e
Abdulrahman <ambadran717@hotmail.com>
2022-09-15 13:51:13 +0200

    Fixed non updating CLI bug
    Change-Id: Ibb3bcdcd8cbeb42e9922570090ee0a934580266c

--- auto_analysis.py
+++ auto_analysis.py
@@ -22,7 +22,7 @@ __version__ = '0.1.0'
 
 ########################################################################################################################################
 
-print('new_new222')
+
 
 ########################################################################################################################################
 ### imports
@@ -3073,7 +3073,6 @@ def read_assign_all_CSVs():
 	'''
 	reads and assigns all csvs to internal class variable cls.all_objects
 	'''
-	print('new_internal test33333')
 	Component.get_all_from_csv(assign_class_variable=True, hash_key='ID')
 
 	DetailedDesign.get_all_from_csv(assign_class_variable=True, hash_key='ID')
